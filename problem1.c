3d30a6cef55a1c64fb1c33d5bf9af418d3938e29
spikecowboybebop <132824450+spikecowboybebop@users.noreply.github.com>
2023-05-31 10:19:43 +0600

    Add files via upload

---
+++ problem1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+typedef struct Shape {
+    double length;
+    double width;
+};
+int findArea(struct Shape R) {
+    double area = R.length * R.width;
+    return area;
+}
+int findPerimeter(struct Shape R) {
+    double perimeter = 2 * (R.length + R.width);
+    return perimeter;
+}
+int main () {
+    struct Shape R;
+
+    printf("Enter length: ");
+    scanf("%lf", &R.length);
+    printf("Enter width: ");
+    scanf("%lf", &R.width);
+
+    printf("The area is %d\n", findArea(R));
+    printf("The perimeter is %d", findPerimeter(R));
+    return 0;
+}
